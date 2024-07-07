eebf7a9a268524c64d727dcc9f60afcf432913fb
Gerardo Guevara <gerardoguevara64@gmail.com>
2024-07-07 12:27:11 -0600

    feat: Add authentication module and controller
    The code changes include adding an authentication module and controller. This allows users to register, login, and verify their accounts. The module is imported into the main app module, and the controller handles the corresponding API endpoints.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProductModule } from './product/product.module';
 import { OrdersModule } from './orders/orders.module';
 import { NastModule } from './transports/nast.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ProductModule, OrdersModule, NastModule],
+  imports: [ProductModule, OrdersModule, NastModule, AuthModule],
   controllers: [],
   providers: [],
 })
