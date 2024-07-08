43e63f0f7abd60d9ccad65b96cc434dd3181ea22
Gerardo Guevara <gerardoguevara64@gmail.com>
2024-07-07 19:34:11 -0600

    feat: Add error handling to register, login, and verify endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,9 @@
 
 import { Controller, Post, Body, Inject, Get } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { NATS_SERVICE } from 'src/config';
 import { LoginUserDto, RegisterUserDto } from './dto';
+import { catchError } from 'rxjs';
 
 @Controller('auth')
 export class AuthController {
@@ -10,16 +11,29 @@ export class AuthController {
 
   @Post('/register')
   register(@Body() registerUserDto: RegisterUserDto) {
-    return this.client.send('auth.register.user', registerUserDto);
+    return this.client.send('auth.register.user', registerUserDto)
+      .pipe(
+        catchError((error) => {
+          throw new RpcException({ status: 400, message: error.message });
+        })
+      );
   }
   @Post('/login')
   login(@Body() loginUserDto: LoginUserDto) {
-    return this.client.send('auth.login.user', loginUserDto);
+    return this.client.send('auth.login.user', loginUserDto).pipe(
+      catchError((error) => {
+        throw new RpcException({ status: 400, message: error.message });
+      })
+    );;
   }
 
   @Get('/verify')
   verify() {
-    return this.client.send('auth.verify.user', {});
+    return this.client.send('auth.verify.user', {}).pipe(
+      catchError((error) => {
+        throw new RpcException({ status: 400, message: error.message });
+      })
+    );;
   }
 
 
